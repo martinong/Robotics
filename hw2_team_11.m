d5ad079caf9d1d6803192317915716c58e7ba2f9
Martin Ong <mo2454@columbia.edu>
2015-10-14 01:31:56 -0400

    HW2 complete: still need to plot orientation for extra credit though

--- hw2_team_11.m
+++ hw2_team_11.m
@@ -15,25 +15,34 @@ function  hw2_team_11(serPort)
 displacement = [0,0];                               % x and y displacement from first wall bump
 a = 0;                                              % angle change since first wall bump
 Total_Distance = 0;
+fig = figure();                                     % Figure for plotting path
+hold on;
+x=0:4;
+y = zeros(size(x));
+plot(x, y, 'g');
 
-display('hi');
-while (~isequal(displacement, [0,4]))
-    display('inside loop');
+% Loop until target is reached
+while sqrt((displacement(1)-4)^2 + displacement(2)^2) > 0.3 || Total_Distance < 1
+    [a, displacement, Total_Distance] = update(a, displacement, Total_Distance, serPort);
+    
     [ BumpRight, BumpLeft, WheelDropRight, WheelDropLeft, WheelDropCastor, BumpFront] = BumpsWheelDropsSensorsRoomba(serPort);
-    WallSensor = WallSensorReadRoomba(serPort);                 % Read Wall Sensor, Requires WallsSensorReadRoomba file
     isCurrentlyBumped = BumpRight || BumpLeft || BumpFront;     % Check if the robot is in a bumped state
     
-    while (~isCurrentlyBumped)
+    % Go straight until wall bump
+    if (~isCurrentlyBumped)
+        plot(displacement(1), displacement(2), 'bo');             % Plots path
+        
         SetFwdVelRadiusRoomba(serPort, 0.2, inf);
-        [ BumpRight, BumpLeft, WheelDropRight, WheelDropLeft, WheelDropCastor, BumpFront] = BumpsWheelDropsSensorsRoomba(serPort);
-        isCurrentlyBumped = BumpRight || BumpLeft || BumpFront;
-        [a, displacement, Total_Distance] = update(a, displacement, Total_Distance, serPort);
         pause(0.05);
+    else
+        % Follow wall until m-line reached
+        [displacement, a] = followWall(serPort, displacement, a);
     end
-    display('follow wall');
-    followWall(serPort, displacement, a);
-end 
-display('done');
+    
+end
+
+SetFwdVelRadiusRoomba(serPort, 0, 2);      % Stop the Robot
+
 end
 
 function [displacement, a] = followWall(serPort, displacement, a)
@@ -42,14 +51,10 @@ function [displacement, a] = followWall(serPort, displacement, a)
 Initial_Distance = DistanceSensorRoomba(serPort);   % Get the Initial Distance
 Total_Distance = 0;                                 % Initialize Total Distance
 hasBeenBumped = false;                              % Has the robot hit a wall yet or still looking for the first
-% fig = figure();                                   % Figure for plotting path
-% hold on;
-displacements = []
+
 % Continue until the robot is sufficiently close to where it initially hit the wall and has travelled far enough
-while sqrt(displacement(1)^2 + displacement(2)^2) > 0.25 || Total_Distance < .2 || abs(displacement(1)) < 0.3
-    displacements = [displacement(1) displacement];
-    min(displacements)
-%     plot(displacement(1), displacement(2), 'bo');             % Plots path
+while sqrt(displacement(1)^2 + displacement(2)^2) > 0.25 || Total_Distance < .2
+    plot(displacement(1), displacement(2), 'bo');             % Plots path
     [ BumpRight, BumpLeft, WheelDropRight, WheelDropLeft, WheelDropCastor, BumpFront] = BumpsWheelDropsSensorsRoomba(serPort); % Read Bumpers
     WallSensor = WallSensorReadRoomba(serPort);                 % Read Wall Sensor, Requires WallsSensorReadRoomba file
 
@@ -80,10 +85,27 @@ while sqrt(displacement(1)^2 + displacement(2)^2) > 0.25 || Total_Distance < .2 
         SetFwdVelRadiusRoomba(serPort, 0.1, -0.2);
         [a, displacement, Total_Distance] = update(a, displacement, Total_Distance, serPort); 
     end
+    
+    % Reaches m-line
+    if (abs(displacement(2)) < 0.15 && Total_Distance > 0.2)
+        if (displacement(1) < 4)
+            turnAngle(serPort, 0.1, (-a)./(pi/180));
+            break;  % Stop wall following
+        elseif (a > 0) % if robot is moving towards the left
+            turnAngle(serPort, 0.1, (a)./(pi/180));
+            break;  % Stop wall following
+        end
+        % if robot is moving towards the right, keep following the wall
+        % (ie. do nothing)
+    end
+    
     pause(0.05);
 end
 
-SetFwdVelRadiusRoomba(serPort, 0, 2);                                       % Stop the Robot
+if (~(sqrt(displacement(1)^2 + displacement(2)^2) > 0.25 || Total_Distance < .2))
+    SetFwdVelRadiusRoomba(serPort, 0, 2);      % Stop the Robot
+    display('I AM STUCK. :(');
+end
 
 end
 
