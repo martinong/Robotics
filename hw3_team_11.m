e915539253d3a6ac2bde3a2c1a0375568673b5c7
Alice Chang <avc2120@columbia.edu>
2015-10-25 00:24:38 -0400

    first draft

--- hw3_team_11.m
+++ hw3_team_11.m
@@ -9,46 +9,61 @@
 % 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+
+
 %% main function
 function  hw3_team_11(serPort)
-    arr = zeros(1,1);
+    global time; time = tic;                                      % time since last update
+    global keySet; keySet =   {};
+    global valueSet; valueSet = [];
+    global map; map = containers.Map(keySet,valueSet)
+    global displacement; displacement = [0,0];                               % x and y displacement from first wall bump
+    global a; a = 0;                                              % angle change since first wall bump
+    global Total_Distance; Total_Distance = 0;
     %spiral until hit
-    spiral(serPort, arr);
-    %wall follow until original
-    %wallFollow(serPort);
-    %random bounce
-    randomBounce(serPort, arr);
-    rect(arr);
+    
+    spiral(serPort);
+    while(toc(time) < 300)
+        if(isKey(map, [x,y]) && map([x,y]) == 2)
+            randomBounce(serPort);
+        end
+        if(~isKey(map, [x,y]))
+            wallFollow(serPort);
+        end
+    end
+%     rect(map);
 end
 
-function spiral(serPort, arr)
-    [ BumpRight, BumpLeft, WheelDropRight, WheelDropLeft, WheelDropCastor, BumpFront] = BumpsWheelDropsSensorsRoomba(serPort);
+function spiral(serPort)
+
     bumped = false;
     radius = 0.05;
     while (~bumped)
-        [ BumpRight, BumpLeft, WheelDropRight, WheelDropLeft, WheelDropCastor, BumpFront] = BumpsWheelDropsSensorsRoomba(serPort);
+        [ BumpRight, BumpLeft, ~, ~, ~, BumpFront] = BumpsWheelDropsSensorsRoomba(serPort);
         SetFwdVelRadiusRoomba(serPort, 0.2, radius);
         pause(0.05);
         bumped = BumpRight || BumpLeft || BumpFront;
         radius = min(radius + 0.005, 2);  
+        update(serPort);
+        if(~isKey(map, [x,y]))
+            map = updateMap(1);
+        end
     end
     pause(0.1);
+
 end
 
-function WallFollow(serPort, arr)
+function WallFollow(serPort)
 % Variable Declaration
-Initial_Distance = DistanceSensorRoomba(serPort);   % Get the Initial Distance
-Total_Distance = 0;                                 % Initialize Total Distance
 hasBeenBumped = false;                              % Has the robot hit a wall yet or still looking for the first
-displacement = [0,0];                               % x and y displacement from first wall bump
-a = 0;                                              % angle change since first wall bump
+
 % fig = figure();                                   % Figure for plotting path
 % hold on;
 
 % Continue until the robot is sufficiently close to where it initially hit the wall and has travelled far enough
 while sqrt(displacement(1)^2 + displacement(2)^2) > 0.25 || Total_Distance < 1
 %     plot(displacement(1), displacement(2), 'bo');             % Plots path
-    [ BumpRight, BumpLeft, WheelDropRight, WheelDropLeft, WheelDropCastor, BumpFront] = BumpsWheelDropsSensorsRoomba(serPort); % Read Bumpers
+    [ BumpRight, BumpLeft, ~, ~, ~, BumpFront] = BumpsWheelDropsSensorsRoomba(serPort); % Read Bumpers
     WallSensor = WallSensorReadRoomba(serPort);                 % Read Wall Sensor, Requires WallsSensorReadRoomba file
 
     isCurrentlyBumped = BumpRight || BumpLeft || BumpFront;     % Check if the robot is in a bumped state
@@ -67,18 +82,19 @@ while sqrt(displacement(1)^2 + displacement(2)^2) > 0.25 || Total_Distance < 1
         % While the robot is being bumped, turn left until it is no longer being bumped.
         hasBeenBumped = true;
         turnAngle(serPort, 0.1, 20);
-        [a, displacement, Total_Distance] = update(a, displacement, Total_Distance, serPort);
+        update(serPort);
     elseif (WallSensor)
         % If it is against a wall and not being bumped, continue moving forward.
         SetFwdVelRadiusRoomba(serPort, 0.2, inf);
-        [a, displacement, Total_Distance] = update(a, displacement, Total_Distance, serPort);
+        update(serPort);
     else
         % If there is no wall to the right, make a half-circle turn (round the corner) to the right looking
         % for another wall.
         SetFwdVelRadiusRoomba(serPort, 0.1, -0.2);
-        [a, displacement, Total_Distance] = update(a, displacement, Total_Distance, serPort); 
+        update(serPort);
     end
     pause(0.05);
+    updateMap(2);
 end
 
 SetFwdVelRadiusRoomba(serPort, 0, 2);                                       % Stop the Robot
@@ -86,24 +102,27 @@ SetFwdVelRadiusRoomba(serPort, 0, 2);                                       % St
 end
 
 
-function randomBounce(serPort, arr)
+function randomBounce(serPort)
     [ BumpRight, BumpLeft, WheelDropRight, WheelDropLeft, WheelDropCastor, BumpFront] = BumpsWheelDropsSensorsRoomba(serPort);
     isCurrentlyBumped = BumpRight || BumpLeft || BumpFront;
-    time = tic;
-    while(toc(time) <= 600)
-        [ BumpRight, BumpLeft, WheelDropRight, WheelDropLeft, WheelDropCastor, BumpFront] = BumpsWheelDropsSensorsRoomba(serPort);
-        isCurrentlyBumped = BumpRight || BumpLeft || BumpFront;
-        if (isCurrentlyBumped)
-            randomAngle = rand * 180 + 90;
-            turnAngle(serPort, 0.4, randomAngle);
-        else
-            SetFwdVelRadiusRoomba(serPort, 0.4, inf);
-        end
-        pause(0.05);
+    
+    [ BumpRight, BumpLeft, WheelDropRight, WheelDropLeft, WheelDropCastor, BumpFront] = BumpsWheelDropsSensorsRoomba(serPort);
+    isCurrentlyBumped = BumpRight || BumpLeft || BumpFront;
+    if (isCurrentlyBumped)
+        randomAngle = rand * 180 + 90;
+        turnAngle(serPort, 0.4, randomAngle);
+    else
+        SetFwdVelRadiusRoomba(serPort, 0.4, inf);
     end
+    pause(0.05);
+    update(serPort);
+    if(~isKey(map, [x,y]))
+        updateMap(1);
+    end
+
 end
 % Update the total distance travelled and displacement.
-function [a, displacement, Total_Distance] = update(a, displacement, Total_Distance, serPort)
+function update(serPort)
     d = DistanceSensorRoomba(serPort);
     a = a + AngleSensorRoomba(serPort);
     [dr,dc] = size(d);
@@ -127,4 +146,14 @@ for i=1:size(m, 1)
         end
     end
 end
+end
+
+function updateMap(val)
+    diameter = 0.4;
+    x = ceil(displacement(1)/diameter);
+    y = ceil(displacement(2)/diameter);
+    if(~isKey(map, [x,y]) || map([x,y]) ~= val)
+    	time = tic;
+    end
+    map([x,y]) = val;
 end
