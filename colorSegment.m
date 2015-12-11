5bafa97994528a1d0f91cdbfb3bd7ada69eac75e
Phil Godzin <pgg2105@columbia.edu>
2015-12-11 18:15:31 -0500

    updated colorSegment

--- colorSegment.m
+++ colorSegment.m
@@ -1,4 +1,4 @@
-function result = colorSegment()
+function [maxArea, obj_x] = colorSegment()
 
 first_img = imread('red2.png');
 imgs = cell(2, 1);
@@ -21,6 +21,9 @@ sHigh = s(y, x) + .2;
 vLow = v(y, x) - .2;
 vHigh = v(y, x) + .2;
 
+pastX = 0;
+pastArea = 0;
+
 for i = 1:size(imgs, 1)
     hsv = rgb2hsv(imread(imgs{i}));
     h = hsv(:,:,1);
@@ -28,24 +31,42 @@ for i = 1:size(imgs, 1)
     v = hsv(:,:,3);
 
     hMask = (h >= hLow) & (h <= hHigh);
-    imshow(hMask);
+    %imshow(hMask);
     sMask = (s >= sLow) & (s <= sHigh);
-    imshow(sMask);
+    %imshow(sMask);
     vMask = (v >= vLow) & (v <= vHigh);
-    imshow(vMask);
+    %imshow(vMask);
     objMask = uint8(hMask & sMask & vMask);
     imshow(objMask);
 
     props = regionprops(bwlabel(objMask), 'area');
-    maxArea = max([props.Area])
+    maxArea = max([props.Area]);
     minObjArea = maxArea - 1;
     objMask = uint8(bwareaopen(objMask, minObjArea));
-    str_el = strel('disk', 10);
+    str_el = strel('disk', 25);
     objMask = imclose(objMask, str_el);
     imshow(objMask, []);
     hold on;
     props = regionprops(bwlabel(objMask), 'Centroid');
-    centroid = props.Centroid
+    centroid = props.Centroid;
+    obj_x = centroid(1);
     plot(centroid(1), centroid(2), 'r*');
+    if (pastX ~= 0 && pastArea ~= 0)
+        moveRobot(obj_x, maxArea, pastX, pastArea);
+    end
+    pastX = obj_x;
+    pastArea = maxArea;
+    
 end
+end
+
+function moveRobot(currX, currArea, pastX, pastArea)
+
+diffX = pastX - currX
+% got bigger -> negative, smaller -> positive
+diffArea = (pastArea - currArea)/pastArea
+
+%SetFwdVelRadiusRoomba(serPort, diffArea/10, 2);
+%turnAngle(serPort, 0.1, randomAngle);
+
 end
